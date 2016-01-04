d08ee68905327d53a62c3796bddf153f3b00e929
Dan Oneata <dan.oneata@gmail.com>
2016-01-04 21:41:51 +0200

    ENH Use a linear algebra library

--- CheckGrad.hs
+++ CheckGrad.hs
@@ -1,46 +1,45 @@
-import Control.Monad.Random
+import Control.Monad.Random (getRandoms, MonadRandom)
 
-type Weights = [Double]
+import Numeric.LinearAlgebra ((|>), sumElements, norm_2)
+import Numeric.LinearAlgebra.Data (fromList, toList, size, R, Vector)
 
 eps = 10 ** (-8)
 
-updateList :: [a] -> Int -> a -> [a]
-updateList list index newElem =
-    zipWith match list [0..]
+updateList :: Vector R -> Int -> R -> Vector R
+updateList vector index newElem =
+    fromList $ zipWith match (toList vector) [0..]
   where
     match elem currentIndex =
       if currentIndex == index
       then newElem
       else elem
 
-norm :: Weights -> Double
-norm xs = sqrt $ sum $ map (**2) xs
-
-checkGrad :: MonadRandom m => (Weights -> Double) -> (Weights -> Weights) -> m (Weights, Weights)
+checkGrad :: MonadRandom m => (Vector R -> R) -> (Vector R -> Vector R) -> m (Vector R, Vector R)
 checkGrad f f' =
-  do
-    -- TODO: ensure neither f nor f' depend on weights length.
-    randomWeights <- take 10 <$> getRandoms
-    let perturb elem index op = f $ updateList randomWeights index (elem `op` eps)
-    let numericalDerivative = zipWith (\elem index -> (perturb elem index (+) - perturb elem index (-)) / (2 * eps)) randomWeights [0..]
-    let estimatedDerivative = f' randomWeights
-    let diffNorm = norm $ zipWith (-) numericalDerivative estimatedDerivative
-    let sumNorm  = norm $ zipWith (+) numericalDerivative estimatedDerivative
-    -- a list of random weights
-    -- compute derivative for each of them
-    -- compare with result of f' for that particular weight
-    -- return $ diffNorm / sumNorm
-    return (numericalDerivative, estimatedDerivative)
+   do
+     -- TODO: ensure neither f nor f' depend on weights length.
+     randomWeights <- fromList <$> take 10 <$> getRandoms
+     let perturb elem index op = f $ updateList randomWeights index (elem `op` eps)
+     let numericalDerivative = fromList $ zipWith (\elem index -> (perturb elem index (+) - perturb elem index (-)) / (2 * eps)) (toList randomWeights) [0..]
+     let estimatedDerivative = f' randomWeights
+     let diffNorm = norm_2 $ numericalDerivative - estimatedDerivative
+     let sumNorm  = norm_2 $ numericalDerivative + estimatedDerivative
+--     -- a list of random weights
+--     -- compute derivative for each of them
+--     -- compare with result of f' for that particular weight
+--     -- return $ diffNorm / sumNorm
+     return (numericalDerivative, estimatedDerivative)
 
+-- Some simple functions and their gradients
 
-f1 :: [Double] -> Double
-f1 = sum
+f1 :: Vector R -> R
+f1 v = sumElements v
 
-f1' :: [Double] -> [Double]
-f1' = map (\_ -> 1)
+f1' :: Vector R -> Vector R
+f1' v = size v |> repeat 1
 
-f2 :: [Double] -> Double
-f2 = sum . map (**2)
+f2 :: Vector R -> R
+f2 v = sumElements $ v ** 2
 
-f2' :: [Double] -> [Double]
-f2' = map (2*)
+f2' :: Vector R -> Vector R
+f2' v = 2 * v
